688726fe3bd30beb3d8a892070acc67ff23aba66
Melina Kamunen <melina.kamunen@edu.turkuamk.fi>
2022-10-31 11:01:32 +0200

    T4JP-46 try to push jira

--- questionnaire/src/App.js
+++ questionnaire/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> allright
+          Edit <code>src/App.js</code> allrigssht
         </p>
         <a
           className="App-link"
